83f8cdeca63ed11651f95a2f4441e579bb0aea1e
msantosEmtech <mario.santos@emtech.digital>
2022-08-16 15:05:53 -0500

    carga de proyecto inicial

---
+++ application/controllers/Home.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Home extends CI_Controller
+{
+
+    public function __construct()
+    {
+        header('Access-Control-Allow-Origin: *');
+        parent::__construct();
+
+        //Zona horaria
+        date_default_timezone_set('America/Mexico_City');
+    }
+
+    public function index()
+    {
+        $this->load->view('home/index');
+    }
+}
---
+++ application/views/customError404.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="es-MX">
+
+<head>
+    <!-- ========== Meta Tags ========== -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Upselling | Salesforce">
+
+    <!-- ========== Page Title ========== -->
+    <title>Upselling | Salesforce</title>
+
+    <!-- ========== Favicon Icon ========== -->
+    <!-- <link rel="shortcut icon" href="assets/img/favicon.png" type="image/x-icon"> -->
+
+    <!-- ========== Start Stylesheet ========== -->
+    <link href="<?= base_url('assets/css/bootstrap.min.css') ?>" rel="stylesheet" />
+    <link href="<?= base_url('assets/css/font-awesome.min.css') ?>" rel="stylesheet" />
+    <link href="<?= base_url('assets/css/themify-icons.css') ?>" rel="stylesheet" />
+    <link href="<?= base_url('assets/css/flaticon-set.css') ?>" rel="stylesheet" />
+    <link href="<?= base_url('assets/css/magnific-popup.css') ?>" rel="stylesheet" />
+    <link href="<?= base_url('assets/css/owl.carousel.min.css') ?>" rel="stylesheet" />
+    <link href="<?= base_url('assets/css/owl.theme.default.min.css') ?>" rel="stylesheet" />
+    <link href="<?= base_url('assets/css/animate.css') ?>" rel="stylesheet" />
+    <link href="<?= base_url('assets/css/bootsnav.css') ?>" rel="stylesheet" />
+    <link href="<?= base_url('assets/css/style.css') ?>" rel="stylesheet">
+    <link href="<?= base_url('assets/css/responsive.css') ?>" rel="stylesheet" />
+    <!-- ========== End Stylesheet ========== -->
+
+    <!-- ========== Google Fonts ========== -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;600;700;800&display=swap"
+        rel="stylesheet">
+
+</head>
+
+<body>
+
+    <!-- Preloader Start -->
+    <div class="se-pre-con"></div>
+    <!-- Preloader Ends -->
+
+    <!-- Start 404 
+    ============================================= -->
+    <div class="error-page-area overflow-hidden default-padding">
+        <!-- Shape Fixed Rotation -->
+        <div class="shape">
+            <img src="<?= base_url('assets/img/shape/4.png') ?>" alt="Thumb">
+        </div>
+        <!-- Dhape Fixed Rotation -->
+        <div class="container">
+            <div class="row align-center">
+                <div class="col-lg-6 thumb">
+                    <img src="<?= base_url('assets/img/illustration/11.png') ?>" alt="Thumb">
+                </div>
+                <div class="col-lg-6">
+                    <div class="error-box">
+                        <h1>404</h1>
+                        <h2>Ups esta pagina no se encuentra!</h2>
+                        <p>
+                            estas intentando entrar a una pagina que no existe, revisa el nombre en la url que sea
+                            correcto, y vuelve a intentar.
+                        </p>
+                        <a class="btn circle btn-md btn-gradient" href="<?= base_url('Home') ?>">volver a home <i
+                                class="fas fa-plus"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End 404 -->
+
+    <!-- jQuery Frameworks
+    ============================================= -->
+    <script src="<?= base_url('assets/js/jquery-1.12.4.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/popper.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/bootstrap.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/equal-height.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/jquery.appear.js') ?>"></script>
+    <script src="<?= base_url('assets/js/jquery.easing.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/jquery.magnific-popup.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/modernizr.custom.13711.js') ?>"></script>
+    <script src="<?= base_url('assets/js/owl.carousel.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/wow.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/progress-bar.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/isotope.pkgd.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/imagesloaded.pkgd.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/count-to.js') ?>"></script>
+    <script src="<?= base_url('assets/js/YTPlayer.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/circle-progress.js') ?>"></script>
+    <script src="<?= base_url('assets/js/bootsnav.js') ?>"></script>
+    <script src="<?= base_url('assets/js/main.js') ?>"></script>
+
+</body>
+
+</html>
